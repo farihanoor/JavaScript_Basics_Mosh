25d2c468a342274e8935b926240b3b8f7d9de242
farihanoor <fariha04noor@gmail.com>
2022-08-12 13:42:31 +0600

    Constants in JS

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// Declaring a Variable in JS
-// by default, the variabLes that we declare in JS are UNDEFINED
-
-let name = "Fariha";
-console.log(name);
-
-// Rules for naming a variable:
-// 1. Cannot be a reserved keyword
-// 2. Should be meaningful and descriptive
-// 3. Cannot start with the number
-// 4. Cannot contain a space or hyphen
-// 5. Use camelNotation
-// 6. Variables are case-sensitive
-// 7. Declare each variable in a separate line
+const interestRate = 0.3;
+interestRate = 1;
+// Since, interestRate is a constant, we will see an error on the console
+console.log(interestRate);
