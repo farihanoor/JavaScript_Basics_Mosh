f2d40b1a7e9b77496df611f3bdacc33fde7ae42d
farihanoor <fariha04noor@gmail.com>
2022-08-12 13:39:01 +0600

    Variables

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+// Declaring a Variable in JS
+// by default, the variabLes that we declare in JS are UNDEFINED
+
+let name = "Fariha";
+console.log(name);
+
+// Rules for naming a variable:
+// 1. Cannot be a reserved keyword
+// 2. Should be meaningful and descriptive
+// 3. Cannot start with the number
+// 4. Cannot contain a space or hyphen
+// 5. Use camelNotation
+// 6. Variables are case-sensitive
+// 7. Declare each variable in a separate line
