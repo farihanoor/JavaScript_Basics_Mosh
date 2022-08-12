69e8332250275436ab905e505aa19846a456318b
farihanoor <fariha04noor@gmail.com>
2022-08-12 13:53:02 +0600

    Primitive Types

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const interestRate = 0.3;
-interestRate = 1;
-// Since, interestRate is a constant, we will see an error on the console
-console.log(interestRate);
+// Types of Variables
+/* Two types of variable in JS
+1. Primitives or Value Types
+    i) String
+    ii) Number
+    iii) Booleans
+    iv) undefined
+    v) null
+
+2. Reference Types
+*/
+
+
+//String variable
+let name = 'Fariha'; // string  literal
+
+//Number variable
+let age = 25;
+
+//Boolean variable
+let isApproved = false;
+
+//undefined variable
+let firstName; // by default, the value of the variable is undefined
+let lastName = undefined; // not common
+
+//null variable
+let number = null; // we use 'null' in situations where we want to clear the value
+                   // of a variable
